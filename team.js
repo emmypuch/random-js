a6805dcdf307a7015aff753e241c6fcb562603a9
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2021-03-08 15:20:07 +0100

    updated repo

---
+++ team.js
@@ -0,0 +1,35 @@
+//Local review data
+const reviews = [
+    {
+        id: 1,
+        name: "Susan Doe",
+        job: "Dev Ops",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpG8p_fqSv-mS8Gunee-EpCJlQeplzcVeOCg&usqp=CAU",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis iste praesentium fugit sapiente eveniet repudiandae ipsa omnis facere id temporibus."
+    },
+
+    {
+        id: 2,
+        name: "Anna Doe",
+        job: "Dev Ops",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpG8p_fqSv-mS8Gunee-EpCJlQeplzcVeOCg&usqp=CAU",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis iste praesentium fugit sapiente eveniet repudiandae ipsa omnis facere id temporibus."
+    },
+
+    {
+        id: 3,
+        name: "Love Doe",
+        job: "Dev Ops",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpG8p_fqSv-mS8Gunee-EpCJlQeplzcVeOCg&usqp=CAU",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis iste praesentium fugit sapiente eveniet repudiandae ipsa omnis facere id temporibus."
+    },
+
+    {
+        id: 4,
+        name: "Joy Doe",
+        job: "Dev Ops",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpG8p_fqSv-mS8Gunee-EpCJlQeplzcVeOCg&usqp=CAU",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis iste praesentium fugit sapiente eveniet repudiandae ipsa omnis facere id temporibus."
+    },
+];
+
